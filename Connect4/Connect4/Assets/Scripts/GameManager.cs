be9b0c1e00beb6ea1911de80bb03defa1a0f4eca
AndreiMercieca <74543545+AndreiMercieca@users.noreply.github.com>
2021-05-25 21:12:49 +0200

    Created the winning system
    Created the winning system horizontally
    vertically and diagonally

--- Connect4/Connect4/Assets/Scripts/GameManager.cs
+++ Connect4/Connect4/Assets/Scripts/GameManager.cs
@@ -12,22 +12,17 @@ public class GameManager : MonoBehaviour
     public GameObject player1ghost;
     public GameObject player2ghost;
 
-public int heightOfBoard = 6;
-public int lengthOfBoard = 7;
+    public int heightOfBoard = 6;
+    public int lengthOfBoard = 7;
 
-public GameObject[] spawnLoc;
+    public GameObject[] spawnLoc;
 
-GameObject fallingPiece;
-bool RedDiscturn = true;
+    GameObject fallingPiece;
+    bool RedDiscturn = true;
 
-int[,] boardState; //0 is empty , 1 is RedDisc , 2 is YellowDisc
+    int[,] boardState; //0 is empty , 1 is RedDisc , 2 is YellowDisc
 
-// 0 0 0 0 0 0 0
-// 0 0 0 0 0 0 0
-// 0 0 0 0 0 0 0
-// 0 0 0 0 0 0 0
-// 0 0 0 0 0 0 0
-// 0 0 1 0 2 0 0
+
     
     void Start()
     {
@@ -35,67 +30,78 @@ int[,] boardState; //0 is empty , 1 is RedDisc , 2 is YellowDisc
         //Debug.LogError("Error");
        // Debug.LogWarning("Warning");
     
-    boardState = new int[lengthOfBoard, heightOfBoard];
-    
-    player1ghost.SetActive(false);
-    player2ghost.SetActive(false);
+        boardState = new int[lengthOfBoard, heightOfBoard];
+        
+        player1ghost.SetActive(false);
+        player2ghost.SetActive(false);
     }
 
     public void HoverColumn(int column)
     {
         if (boardState[column, heightOfBoard-1]==0 && fallingPiece == null ||   fallingPiece.GetComponent<Rigidbody>().velocity == Vector3.zero)
-        
-        if (RedDiscturn)
         {
-        player1ghost.SetActive(true);
-            player1ghost.transform.position = spawnLoc[column].transform.position;
-                }else{
-            player2ghost.SetActive(true);
-            player2ghost.transform.position = spawnLoc[column].transform.position;
+            if (RedDiscturn)
+            {
+                player1ghost.SetActive(true);
+                player1ghost.transform.position = spawnLoc[column].transform.position;
+            }
+            else
+            {
+                player2ghost.SetActive(true);
+                player2ghost.transform.position = spawnLoc[column].transform.position;
+            }            
         }
-        }
+    }
 
     public void SelectColumn(int column)
     {
         if (fallingPiece == null ||   fallingPiece.GetComponent<Rigidbody>().velocity == Vector3.zero)
-        
-    //Debug.Log("GameManager Column" + column );
-    TakeTurn(column);
-    }
+        {
+        //Debug.Log("GameManager Column" + column );
+            TakeTurn(column);
+        }
+        }
 
     void TakeTurn(int column)
-    {
+    {   
         if (updateboardState(column))
         {
-        player1ghost.SetActive(false);
-        player2ghost.SetActive(false);
+            player1ghost.SetActive(false);
+            player2ghost.SetActive(false);
 
-
-
-if (RedDiscturn)
-        {
-            fallingPiece = Instantiate(RedDisc,spawnLoc[column].transform.position,Quaternion.identity);
-            fallingPiece.GetComponent<Rigidbody>().velocity = new Vector3(0, 0.1f,0);
-            RedDiscturn = false;
-        }
-        else
-        {
-            Instantiate(YellowDisc,spawnLoc[column].transform.position,Quaternion.identity);
-            fallingPiece.GetComponent<Rigidbody>().velocity = new Vector3(0, 0.1f,0);
-            RedDiscturn = true;
+            if (RedDiscturn)
+            {
+                fallingPiece = Instantiate(RedDisc,spawnLoc[column].transform.position,Quaternion.identity);
+                fallingPiece.GetComponent<Rigidbody>().velocity = new Vector3(0, 0.1f,0);
+                RedDiscturn = false;
+                if (DidWin(1))
+                {
+                    Debug.LogWarning("Player 1  Won!");
+                }
+            }
+            else
+            {
+                fallingPiece = Instantiate(YellowDisc,spawnLoc[column].transform.position,Quaternion.identity);
+                fallingPiece.GetComponent<Rigidbody>().velocity = new Vector3(0, 0.1f,0);
+                RedDiscturn = true;
+                if (DidWin(2))
+                {
+                    Debug.LogWarning("Player 2  Won!");
+                }
+            }
         }
     }
-}
 
-bool updateboardState(int column)
+
+    bool updateboardState(int column)
     {  
-    for (int row = 0; row < heightOfBoard; row++ )
+        for (int row = 0; row < heightOfBoard; row++ )
         {
-        if (boardState[column,row] == 0) //Found empty spot
-        {
-            if(RedDiscturn)
+            if (boardState[column,row] == 0) //Found empty spot
             {
-                boardState[column, row] = 1;
+                if(RedDiscturn)
+                {
+                    boardState[column, row] = 1;
                 }
                 else
                 {
@@ -107,5 +113,63 @@ bool updateboardState(int column)
         }
         Debug.LogWarning("full");
         return false;
+    }        
+
+    bool DidWin(int playerNum)
+    {
+        //Horizontal
+        for (int x = 0; x < lengthOfBoard -3; x++)
+        {
+            for (int y = 0; y < heightOfBoard; y++)
+            {
+                if (boardState [x+0, y] == playerNum && boardState[x+1, y] == playerNum && boardState[x+ 2, y] == playerNum && boardState[x+ 3, y] == playerNum)
+                {
+                    return true;
+                }
+            }
+        }
+        //Vertical
+        for (int x =0; x < lengthOfBoard; x++)
+        {
+            for (int y =0; y < heightOfBoard -3; y++)
+            {
+                if (boardState[x,y] == playerNum && boardState[x, y + 1] == playerNum && boardState[x, y + 2] == playerNum && boardState[x, y + 3] == playerNum)
+                {
+                    return true;
+                }
+            }
+        }
+        //Diagonal Right
+        for (int x =0; x < lengthOfBoard; x++)
+        {
+            for (int y =0; y < heightOfBoard -3; y++)
+            {
+                if (boardState[x,y] == playerNum && boardState[x + 1, y + 1] == playerNum && boardState[x + 2, y + 2] == playerNum && boardState[x + 3, y + 3] == playerNum)
+                {
+                    return true;
+                }
+            }
+        }
+        //Diagonal Left
+        for (int x =0; x < lengthOfBoard; x++)
+        {
+            for (int y =0; y < heightOfBoard -3; y++)
+            {
+                if (boardState[x,y + 3] == playerNum && boardState[x + 1, y + 2] == playerNum && boardState[x + 2, y + 1] == playerNum && boardState[x + 3, y] == playerNum)
+                {
+                    return true;
+                }
+            }
+        }
+            return false;
     }
 }
+
+
+
+
+
+
+
+
+
